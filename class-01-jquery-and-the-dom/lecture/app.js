64d5145ee3953934c85e4425046f8cb76b19cf9d
Dan Schwartz <dpschwartz@gmail.com>
2016-07-18 18:26:05 -0700

    Add lecture code for class01. Unzip class01 starter code

---
+++ class-01-jquery-and-the-dom/lecture/app.js
@@ -0,0 +1,18 @@
+//*****basic selectors*****//
+
+//element
+//class
+//id
+//parent descendent / parent > child
+//attribute
+
+//what does a JQ selector return?
+
+//jq methods
+  //chaining
+  //get/set syntax
+
+//append, remove, clone, data, html, text
+
+// $(document).ready(function() {
+// })
