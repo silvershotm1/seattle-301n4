dfc58fd61cdebb137d00afdc1b903940475d5399
REN BURNETT <renxburnett@gmail.com>
2016-07-19 21:50:50 -0700

    working now

--- class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/starter-code/scripts/article.js
@@ -6,18 +6,19 @@ function Article (opts) {
   this.title = opts.title;
   this.category = opts.category;
   this.author = opts.author;
-  this.authorURL = opts.authorURL;
+  this.authorUrl = opts.authorUrl;
   this.publishedOn = opts.publishedOn;
   this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
 
+  var $newArticle = $('article.template').clone();
+  console.log($newArticle);
   $newArticle.attr('data-category', this.category);
 
   $newArticle.find('a').text(this.author);
-  $newArticle.find('a').attr('href', this.authorURL);
+  $newArticle.find('a').prop('href', this.authorURL);
 
   $newArticle.find('h1').text(this.title);
   $newArticle.attr('pubdate', this.publishedOn);
@@ -28,20 +29,19 @@ Article.prototype.toHtml = function() {
   // the author name and url, the article title and body, and the
   // publication date.
 
-
-
   // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
 
   // Display the date as a relative number of "days ago":
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
 
   $newArticle.append('<hr>');
-
+  
   // TODO: This cloned article is no longer a template, so we should remove that class...
+  $newArticle.removeClass('template');
 
   return $newArticle;
-}
+};
 
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
@@ -49,8 +49,8 @@ rawData.sort(function(a,b) {
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-})
+});
 
 articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+  $('#articles').append(a.toHtml());
 });
