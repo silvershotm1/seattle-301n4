8d44c28c99fe12bac5d68f505929b261783cb946
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-07-21 21:22:57 -0700

    nav is working

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -77,7 +77,12 @@ articleView.handleMainNav = function() {
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-  $('.main-nav').on(/* CODE GOES HERE */);
+  $('.main-nav').on('click', '.tab', function(){
+    var $whereToGo = $(this).data('content');
+    $('.tab-content').hide();
+    $('#' + $whereToGo).show();
+  }
+  );
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
@@ -98,4 +103,5 @@ $(function(){
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
+  articleView.handleMainNav();
 });
