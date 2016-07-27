4f192d29c27044f4b3711293f84f2dfea9ee4740
Nick Jacoby <nickjacoby22@gmail.com>
2016-07-26 20:11:17 -0700

    incorporated handlebars template for replacing jQuery

--- class-04-templates/pair-assignment/nick-michael/scripts/article.js
+++ class-04-templates/pair-assignment/nick-michael/scripts/article.js
@@ -15,15 +15,17 @@ Article.prototype.toHtml = function() {
   //       - Now "compile" your template with Handlebars.
   var source = $('#article-template').html();
   var template = Handlebars.compile(source);
+
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+
   // DONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
   var html = template(this);
-  $('#app').append(html);
+  $('#articles').append(html);
 };
 
 rawData.sort(function(a,b) {
