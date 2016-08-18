87612871ec7f5cc0a180c8637998a4b04bee03c0
Michael Bishop <polishthebishop@gmail.com>
2016-08-17 17:58:58 -0700

    Defined the articleController.js

--- class-11-spa-routing/pair-assignment/michael-dan/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/michael-dan/starter-code/scripts/articleController.js
@@ -1,13 +1,16 @@
 (function(module) {
   var articlesController = {};
 
-  // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  // Done: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  Article.createTable();                        //creates table
+  Article.fetchAll(articleView.initIndexPage);  // function to render table
 
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // Done: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
-  // TODO: Also be sure to hide all the main section elements, and reveal the #articles section:
+  // Done: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
-
+    $('tab-content').hide();
+    $('#about').fadeIn('slow');
   };
 
   module.articlesController = articlesController;
