f008c1128c7e803ae015b0b9a826c8969822f0c3
Michael Bishop <polishthebishop@gmail.com>
2016-08-17 18:45:50 -0700

    Define route.js file. Need to complete function

--- class-11-spa-routing/pair-assignment/michael-dan/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/michael-dan/starter-code/scripts/routes.js
@@ -1,4 +1,10 @@
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
+page('/', articlesConroller.index);       // Calls INDEX page
+page('/about', aboutController.index);    // Calls About page
+page('*', function() {                    // Function to pass pages -- TODO
 
+});
 // TODO: What function do you call to activate page.js? Fire it off now, to execute!
+
+page();   // Calls the page function
