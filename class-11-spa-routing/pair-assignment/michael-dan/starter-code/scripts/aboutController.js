dacef1ef1253cbc839f853023467f0ce16110099
Michael Bishop <polishthebishop@gmail.com>
2016-08-17 17:51:43 -0700

    Define the aboutController.js

--- class-11-spa-routing/pair-assignment/michael-dan/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/michael-dan/starter-code/scripts/aboutController.js
@@ -1,9 +1,10 @@
 (function(module) {
   var aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // Done: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
-
+    $('tab-content').hide();
+    $('#about').fadeIn('slow');
   };
 
   module.aboutController = aboutController;
