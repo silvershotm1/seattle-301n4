1bcdcfaff4b4839f0c1f29e5a03305261eee0dc6
Dan Peters <daniel.r.peters@icloud.com>
2016-08-18 19:35:37 -0700

    fixed issues with index.html

--- class-11-spa-routing/pair-assignment/michael-dan/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/michael-dan/starter-code/scripts/articleController.js
@@ -2,8 +2,8 @@
   var articlesController = {};
 
   // Done: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
-  Article.createTable();                        //creates table
-  Article.fetchAll(articleView.initIndexPage);  // function to render table
+  module.Article.createTable();                        //creates table
+  module.Article.fetchAll(articleView.initIndexPage);  // function to render table
 
   // Done: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
